ebc702220db06b9811947633a91b0156d910512a
kashan-iqbal <kashaniqbal33@gmail.com>
2024-10-20 20:14:30 +0500

    check

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import React, { lazy } from "react";
 import { BrowserRouter } from "react-router-dom";
 import {
   About,
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,13 +9,12 @@ const Hero = () => {
         className={`${styles.paddingX} absolute insert-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}
       >
         <div className="flex flex-col justify-center items-center mt-5">
-          <div className="h-5 w-5 rounded-full bg-[#915eff]" />
+          <div className="h-5 w-5 rounded-full bg-[#441d99]" />
           <div className="w-1 h-40 sm:h-80 violet-gradient " />
         </div>
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}>
-            {" "}
-            Hi, I'm <span className=" text-[#915eff]">Kashan Iqbal</span>{" "}
+            Hi, I'm <span className=" text-[#915eff]">Kashan Iqbal</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100px `}>
             {" "}
