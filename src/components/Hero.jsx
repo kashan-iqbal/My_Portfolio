7695b750ff59dd7ad92e164179cbc3ec18ad250d
kashan-iqbal <kashaniqbal33@gmail.com>
2025-02-04 13:55:54 +0500

    temp

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -16,7 +16,7 @@ const Hero = () => {
 
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}>
-            Hi, I'm <span className="text-[#915EFF]">Kashan</span>
+            Hi, I'm <span className="text-[#915EFF]">kk</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
             Full Stack Developer <br className="sm:block hidden" />
